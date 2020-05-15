40c7580efc0241089ddd6945987fa7d0a5884174
Diiru <diiru@uc.cl>
2020-05-15 16:52:47 -0400

    first commit, filesystem structures

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	if (argc > 2)
+	{
+		printf("Modo de uso: ./crfs <simdisk.bin> \n");
+		return 0;
+	}
+
+	return 0;
+}
---
+++ src/structs.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+
+
+typedef struct block_directory {
+} Directoy;
+
+
+typedef struct block_bitmap {
+} Bitmap;
+
+
+typedef struct block_index {
+} Index;
+
+
+typedef struct block_indirect_simple {
+} Indirect;
+
+
+typedef struct data {
+} Data_Block;
