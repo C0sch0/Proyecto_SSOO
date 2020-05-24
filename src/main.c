f44a7ff33fc51ecb9685973192fdba144d140b63
AntoniaChristensen <achristensen1@uc.cl>
2020-05-23 21:56:21 -0400

    finished mode r for cr_open and updated some structs and functions (init)

--- src/main.c
+++ src/main.c
@@ -4,8 +4,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include<unistd.h>
-#include <math.h>
+#include <unistd.h>
+#include<math.h>
 #include <sys/types.h>
 #include "structs.h"
 
@@ -22,117 +22,11 @@ Entry * entry_init()
   return entrada;
 }
 
-Data_Block * init_datablock(){
-  Data_Block * data = malloc(sizeof(Data_Block));
-  data -> content = malloc(sizeof(char)*8192);
-  return data;
-}
-
-Indirect * init_ind_simple(){
-  Indirect * indirect = malloc(sizeof(Indirect));
-  for(int i = 0; i < 2048 ; i++){
-    indirect -> indirect_blocks_data[i] = malloc(sizeof(int));
-  }
-  return indirect;
-}
-
-Index * init_indice(){
-  Index * indice = malloc(sizeof(Index));
-
-  indice -> indirect_simple = init_ind_simple();
-
-  for(int i = 0; i < 2044 ; i++){
-    indice -> blocks_data[i]= init_datablock();
-  }
-
-  return indice;
-}
-
-crFILE* init_crfile(){
-  crFILE* file = malloc(sizeof(crFILE));
-
-  file -> file_name = malloc(sizeof(char)*29);
-  file -> valid = malloc(sizeof(char)*24);
-  file -> mode = malloc(sizeof(char));
-  file -> indice = init_indice();
-
-  // Donde estamos en el archivo
-  file -> estado = 0;
-  file -> dir = 0;
-
-  return file;
-
-}
-
-void destroy_crfile(crFILE*file){
-  free(file-> file_name);
-  free(file-> valid);
-  free(file-> mode);
-  free(file);
-}
-
-void destroy_data(Data_Block * data){
-  free(data -> content);
-  free(data);
-}
-
-void destroy_ind_dir(Indirect * indirect){
-  for(int i = 0; i < 2048 ; i++){
-    free(indirect -> indirect_blocks_data[i]);
-  }
-  free(indirect);
-}
-
-void destroy_indice(Index * index){
-
-  free(index -> references);
-  free(index -> file_size);
-  //destroy_data(indice -> blocks_data);
-  //destroy_ind_dir(indice -> indirect_simple);
-  free(index);
-}
-
 void cr_mount(char* diskname)
 {
   ruta_archivo = diskname;
 }
 
-
-// https://gist.github.com/ryankshah/bf45790b968540cdabdeef702883ddbb modificada
-int * get_bloque(char * string) {
-    int count = 0;
-    int num_bloque = 0;
-    int stringLength = strlen(string);
-    int mask = 0x80; /* 10000000 */
-    int * bloque = malloc(sizeof(int)*24);
-
-    for(int i = 0; i < stringLength; i++) {
-        mask = 0x80;
-        char c = string[i];
-        int x = 0;
-        while(mask > 0) {
-            char n = (c & mask) > 0;
-            bloque[count] = (int) n;
-            //printf("%i\n", bloque[count]);
-            mask >>= 1; /* move the bit down */
-            count++;
-        }
-    }
-
-    // transformamos ahora los bit al numero de bloque
-    for(int k = 1; k < 24; k++){
-      // no consideramos el 0 ya que ese corresponde al valid bit
-      if(bloque[k] == 1){
-        num_bloque += pow(2,23-k);
-      }
-    }
-
-    free(bloque);
-    //printf("%d\n", num_bloque);
-    return num_bloque;
-}
-
-// No la estamos usando por ahora
 void directorio_append(Directory* bloque, Entry *entrada, int i)
 {
   bloque-> entries[i] = entry_init();
@@ -147,7 +41,8 @@ int cr_exists(unsigned disk, char* filename)
 
   for(int i = 0; i<256;i++){
     if(strncmp(Dir_disk[disk-1]->entries[i]->file_name , filename, 32) ==0 ){
-      printf("existe\n");
+      //printf("existe\n");
+      free(str);
       return 1;
     }else{
 
@@ -159,6 +54,7 @@ int cr_exists(unsigned disk, char* filename)
           // llamo denuevo a la función y busco el nombre en la partición indicada
           valid = cr_exists(atoi(particion), nombre);
           if(valid == 1){
+          free(str);
           return 1;
         }
       }
@@ -167,7 +63,7 @@ int cr_exists(unsigned disk, char* filename)
 }
 
   free(str);
-  printf("no existe\n");
+  //printf("no existe\n");
   return 0;
 }
 
@@ -338,6 +234,180 @@ void destroy_bitmaps()
 	}
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+Data_Block* init_datablock()
+{
+  Data_Block* data = malloc(sizeof(Data_Block));
+  data -> content = malloc(sizeof(unsigned int));
+  return data;
+}
+
+Indirect* init_ind_simple()
+{
+  Indirect* indirect = malloc(sizeof(Indirect));
+  indirect -> indirect_blocks_data = malloc(sizeof(unsigned int)*2048);
+  return indirect;
+}
+
+Index* init_indice()
+{
+  Index* indice = malloc(sizeof(Index));
+  indice->blocks_data = malloc(sizeof(unsigned int)*2044);
+  return indice;
+}
+
+crFILE* init_crfile(){
+  crFILE* file = malloc(sizeof(crFILE));
+  file -> file_name = malloc(sizeof(char)*29);
+  file -> valid = calloc(3, sizeof(char));
+  file -> mode = malloc(sizeof(char));
+  file -> indice = init_indice();
+
+  // Donde estamos en el archivo
+  file -> estado = 0;
+  file -> dir = 0;
+  return file;
+}
+void destroy_indice(Index * index){
+
+  //free(index -> references);
+  //free(index -> file_size);
+  //destroy_data(indice -> blocks_data);
+  //destroy_ind_dir(indice -> indirect_simple);
+	free(index->blocks_data);
+  	free(index);
+}
+
+void destroy_crfile(crFILE*file){
+  free(file-> file_name);
+  free(file-> valid);
+  free(file-> mode);
+  destroy_indice(file->indice);
+  free(file);
+}
+
+void destroy_data(Data_Block * data){
+  free(data -> content);
+  free(data);
+}
+
+// void destroy_ind_dir(Indirect *indirect){
+//   for(int i = 0; i < 2048 ; i++){
+//     free(indirect -> indirect_blocks_data[i]);
+//   }
+//   free(indirect);
+// }
+
+
+
+
+// https://gist.github.com/ryankshah/bf45790b968540cdabdeef702883ddbb modificada
+int get_bloque(char* string) {
+    int count = 0;
+    int num_bloque = 0;
+    int stringLength = 3;
+    //printf(" stringLength %d\n", stringLength);
+    int mask = 0x80; /* 10000000 */
+    int *bloque = malloc(sizeof(int)*24);
+
+    for(int i = 0; i < stringLength; i++) {
+        mask = 0x80;
+        char c = string[i];
+        int x = 0;
+        while(mask > 0) {
+            char n = (c & mask) > 0;
+            bloque[count] = (int) n;
+            //printf("bloque[count]=%i\n", bloque[count]);
+            mask >>= 1; /* move the bit down */
+            count++;
+        }
+    }
+
+    // transformamos ahora los bit al numero de bloque
+	for(int k = 1; k < 24; k++)
+	{
+  	// no consideramos el 0 ya que ese corresponde al valid bit
+  		if(bloque[k] == 1)
+  		{
+  			int ap = 23 - k;
+    	num_bloque += pow(2,ap);
+  		}
+	}
+
+	free(bloque);
+	//printf("%d\n", num_bloque);
+	return num_bloque;	   
+}
+
+int buscar_ref(char* str)
+{
+	int bloque[32];
+    int count = 0;
+    for (int j = 0; j < 4; j++)
+    {
+        int mask = 0x80;
+        while(mask > 0) {
+            char n = (str[j] & mask) > 0;
+            bloque[count] = (int) n;
+            mask >>= 1; /* move the bit down */
+            count++;
+        }
+    }
+    int ref = 0;
+    for(int i = 0; i< 32; i++)
+    {
+    	if(bloque[i]==1)
+    	{
+    		int ar = 31 - i;
+    		int n = pow(2,ar);
+    		ref += n;
+    	}
+    }
+    return ref;														
+}
+
+long buscar_size(char* str)
+{
+	int bloque[64];
+    int count = 0;
+    for (int j = 0; j < 8; j++)
+    {
+        int mask = 0x80;
+        while(mask > 0) {
+            char n = (str[j] & mask) > 0;
+            bloque[count] = (int) n;
+            mask >>= 1; /* move the bit down */
+            count++;
+        }
+    }
+    long siz = 0;
+    for(int i = 0; i< 64; i++)
+    {
+    	if(bloque[i]==1)
+    	{
+    		int ar = 63 - i;
+    		int n = pow(2,ar);
+    		siz += n;
+    	}
+    }
+    return siz;														
+}
+
+
+// No la estamos usando por ahora
+
+
 crFILE* cr_open(unsigned disk, char* filename, char *mode){
 
   char *particion;
@@ -349,52 +419,51 @@ crFILE* cr_open(unsigned disk, char* filename, char *mode){
 
   if(strncmp(mode , "r", 1) == 0){
     int exist;
-    // Cheamos que exista el archivo
+    // Chequeamos que exista el archivo
     exist = cr_exists(disk, filename);
     if(exist == 1){
       // el archivo en el disco si existe creamos el crFILE
-      char *ptr;
+      char* ptr;
       crFILE* file;
       file = init_crfile();
 
       // ahora tengp que buscar el archivo
-      for(int i = 0; i<256;i++){
-
-          if(strncmp(Dir_disk[disk-1]->entries[i]->file_name , filename, 32) ==0 ){
+      for(int i = 0; i<256;i++)
+      {
+      	if(strncmp(Dir_disk[disk-1]->entries[i]->file_name , filename, 32) ==0 )
+      	{
             // si el nombre esta en la partición
-
             // le doy a file los atributos del archivo
             memcpy(file->file_name, Dir_disk[disk-1]->entries[i]->file_name, 29);
             memcpy(file->valid, Dir_disk[disk-1]->entries[i]->number, 3);
-
             // le cambio asigno el modo en el que se abrio
             memcpy(file->mode, mode, 1);
             //memcpy(file->dir, 0, 1);
-
             break;
           }
-          else{
+          else
+          {
             // si el nombre no esta en la partición
             memcpy(str, Dir_disk[disk-1]->entries[i]->file_name, 32);
             particion = strtok(str,"/");
             nombre = strtok(NULL,"/");
 
-            if(nombre!=NULL){
-              if(strncmp(nombre , filename, 32) == 0){
+            if(nombre!=NULL)
+            {
+              if(strncmp(nombre , filename, 32) == 0)
+              {
                 prt = atoi(particion);
-                for(int j = 0; j<256;j++){
+                for(int j = 0; j<256;j++)
+                {
                   // buscamos en la particion
-                  if(strncmp(Dir_disk[prt-1]->entries[j]->file_name , filename, 32)==0){
+                  if(strncmp(Dir_disk[prt-1]->entries[j]->file_name , filename, 32)==0)
+                  {
                     // obtengo el indice del archivo que indica la partición a la que pertence
-
                     memcpy(file->file_name, Dir_disk[prt-1]->entries[j]->file_name, 29);
                     memcpy(file->valid, Dir_disk[prt-1]->entries[j]->number, 3);
-
                     memcpy(file->mode, mode, 1);
-
                     // apunta a otro directorio
                     //memcpy(file->dir, 1, 1);
-
                     break;
                   }
                 }
@@ -403,45 +472,173 @@ crFILE* cr_open(unsigned disk, char* filename, char *mode){
           }
         }
 
-        // Cargamos en bloque indice del archivo a crFILE
-
+        // Cargamos el bloque indice del archivo a crFILE
         // Buscamos el puntero al ruta_archivo--> decodificar valid del filename
         char *st= malloc(sizeof(char)*3);
         int n_bloque_indice;
         char* indice_aux = malloc(sizeof(char)*8192);
 
         memcpy(st, file->valid, 3);
+        //printf("%d\n", *st);
         n_bloque_indice = get_bloque(st);
-        printf("%d\n", n_bloque_indice);
+        //printf("bloque indice %d\n", n_bloque_indice);
 
         //seteamos el archivo en el bloque indice
         fseek(disco, 8192 * n_bloque_indice , SEEK_SET);
 
         // esto tenemos que decodificarlo a los bytes que correspondan por segnmento
         fread(indice_aux, 8192, 1, disco);
-
+        Index* ind= file->indice;
+        char* aux_ref = malloc(sizeof(char)*4);
+        memcpy(aux_ref, indice_aux, 4);
+        ind->references = buscar_ref(aux_ref);
+        free(aux_ref);
+        char* aux_size = malloc(sizeof(char)*8);
+        memcpy(aux_size, &(indice_aux[4]), 8);
+        ind->file_size = buscar_size(aux_size);
+        free(aux_size);
+        for (int i = 0; i < 2044; i++)
+        {
+        	char* aux_ptr = malloc(sizeof(char)*4);
+        	int q = 12+4*i;
+        	memcpy(aux_ptr, &(indice_aux[q]), 4);
+        	ind->blocks_data[i] = buscar_ref(aux_ptr);
+        	free(aux_ptr);
+        }
+        char* aux_ind = malloc(sizeof(char)*4);
+        memcpy(aux_ind, &(indice_aux[8188]), 4);
+        ind->indirect_simple = buscar_ref(aux_ind);
+        free(aux_ind);
         free(st);
+        free(str);
         free(indice_aux);
+        fclose(disco);
+        return file;
     }
     else{
       // exist es 0
       printf("El archivo no existe\n");
+      free(str);
+      fclose(disco);
+      return NULL;
       }
     }
-    else if(strncmp(mode , "w", 1) ==0){
-      printf("Mode escritura\n");
-    }
+    else if(strncmp(mode , "w", 1) ==0)
+    {
+    //printf("Mode escritura\n");
+    	int exist;
+    // Chequeamos que exista el archivo
+    	exist = cr_exists(disk, filename);
+    	if(exist == 1)
+    	{
+      	// el archivo en el disco si existe creamos el crFILE
+      		char* ptr;
+      		crFILE* file;
+      		file = init_crfile();
 
-free(str);
+      		// ahora tengp que buscar el archivo
+      		for(int i = 0; i<256;i++)
+      		{
+      			if(strncmp(Dir_disk[disk-1]->entries[i]->file_name , filename, 32) ==0 )
+      			{
+            		// si el nombre esta en la partición
+            		// le doy a file los atributos del archivo
+            		memcpy(file->file_name, Dir_disk[disk-1]->entries[i]->file_name, 29);
+		            memcpy(file->valid, Dir_disk[disk-1]->entries[i]->number, 3);
+		            // le cambio asigno el modo en el que se abrio
+		            memcpy(file->mode, mode, 1);
+		            //memcpy(file->dir, 0, 1);
+		            break;
+          		}
+         	 	else
+          		{
+		            // si el nombre no esta en la partición
+		            memcpy(str, Dir_disk[disk-1]->entries[i]->file_name, 32);
+		            particion = strtok(str,"/");
+		            nombre = strtok(NULL,"/");
+
+		            if(nombre!=NULL)
+            		{
+		            	if(strncmp(nombre , filename, 32) == 0)
+		              	{
+		                	prt = atoi(particion);
+		                	for(int j = 0; j<256;j++)
+		                	{
+		                  		// buscamos en la particion
+		                  		if(strncmp(Dir_disk[prt-1]->entries[j]->file_name , filename, 32)==0)
+		                  		{
+				                    // obtengo el indice del archivo que indica la partición a la que pertence
+				                    memcpy(file->file_name, Dir_disk[prt-1]->entries[j]->file_name, 29);
+				                    memcpy(file->valid, Dir_disk[prt-1]->entries[j]->number, 3);
+				                    memcpy(file->mode, mode, 1);
+				                    // apunta a otro directorio
+                    				//memcpy(file->dir, 1, 1);
+                    				break;
+                  				}
+                			}
+              			}
+            		}
+          		}
+        	}
+
+        	// Cargamos el bloque indice del archivo a crFILE
+       		// Buscamos el puntero al ruta_archivo--> decodificar valid del filename
+	        char *st= malloc(sizeof(char)*3);
+	        int n_bloque_indice;
+	        char* indice_aux = malloc(sizeof(char)*8192);
+	        memcpy(st, file->valid, 3);
+	        n_bloque_indice = get_bloque(st);
+	        
+	        //seteamos el archivo en el bloque indice
+	        fseek(disco, 8192 * n_bloque_indice , SEEK_SET);
+
+	        // esto tenemos que decodificarlo a los bytes que correspondan por segnmento
+	        fread(indice_aux, 8192, 1, disco);
+	        Index* ind= file->indice;
+	        char* aux_ref = malloc(sizeof(char)*4);
+	        memcpy(aux_ref, indice_aux, 4);
+	        ind->references = buscar_ref(aux_ref);
+	        free(aux_ref);
+	        char* aux_size = malloc(sizeof(char)*8);
+	        memcpy(aux_size, &(indice_aux[4]), 8);
+	        ind->file_size = buscar_size(aux_size);
+	        free(aux_size);
+        	for (int i = 0; i < 2044; i++)
+        	{
+	        	char* aux_ptr = malloc(sizeof(char)*4);
+	        	int q = 12+4*i;
+	        	memcpy(aux_ptr, &(indice_aux[q]), 4);
+	        	ind->blocks_data[i] = buscar_ref(aux_ptr);
+	        	free(aux_ptr);
+        	}								
+       		 char* aux_ind = malloc(sizeof(char)*4);
+	        memcpy(aux_ind, &(indice_aux[8188]), 4);
+	        ind->indirect_simple = buscar_ref(aux_ind);
+	        free(aux_ind);
+	        free(st);
+	        free(str);
+	        free(indice_aux);
+	        fclose(disco);
+        	return file;
+    	}
+    	else
+    	{
+      		// exist es 0
+      		//tengo que buscarle espacio al bloque indice
+      	}
+    }
 }
 
 int main() {
   cr_mount("simdiskfilled.bin");
   create_dir_blocks();
-  cr_exists(1, "Baroque.mp3");
-  cr_ls(1);
-  cr_open(1, "Baroque.mp3","r");
+  //cr_exists(1, "Baroque.mp3");
+  //cr_ls(1);
+  crFILE* arch = cr_open(3, "Program in C.mkv","r");
+  printf("%ld\n", arch->indice->file_size);
+  destroy_crfile(arch);
   destroy_directories();
+
   return 0;
 }
 
--- src/structs.h
+++ src/structs.h
@@ -5,20 +5,20 @@
 
 
 typedef struct block_indirect_simple {
-  unsigned int *indirect_blocks_data[2048]; //array de punteros a data blocks
+  unsigned int *indirect_blocks_data; //array de punteros a data blocks
 } Indirect;
 
 
 typedef struct data {
-  char *content; // array del contenido del bloque
+  unsigned int *content; // numero de bloque donde esta el contenido
 } Data_Block;
 
 
 typedef struct block_index {
   unsigned int references; //numero de hardlinks del archivo
   unsigned long file_size; //tamaño
-  Data_Block *blocks_data[2044]; //array de punteros a data blocks
-  Indirect *indirect_simple; //puntero a bloque de indireccionamiento simple
+  unsigned int *blocks_data; //array de punteros a data blocks
+  unsigned int indirect_simple; //puntero a bloque de indireccionamiento simple
 } Index;
 
 /// estructura entrada  directorio auxiliar-->tami
@@ -57,5 +57,4 @@ typedef struct cr_file {
   int  estado;
   int  dir;
   Index* indice;
-
 } crFILE;
