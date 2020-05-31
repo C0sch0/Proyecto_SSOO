146910fd1a1afcda01d6d2674741ec60f9c8a883
Diiru <diiru@uc.cl>
2020-05-31 18:12:41 -0400

    load tests

--- src/crfs/main.c
+++ src/crfs/main.c
@@ -20,11 +20,29 @@ int main(int argc, char *argv[]) {
     printf("Disk not mounted");
     return 0;
   }
+
   create_dir_blocks();
   create_cr_bitmaps();
 
+  printf("1 - En la particion 3 tenemos:\n");
+  printf("------------------ CR LS Particion 3 ------------------ \n");
+  cr_ls(3);
 
-  cr_ls(1);
+  printf("2. Probaremos primero copiando un archivo txt y un .gif a esta particion.\n");
+  printf("3. Archivo .txt copiado\n");
+  printf("4. Archivo .gif copiado\n");
+
+  char file1[40] =  "prueba.txt";
+  char file2[40] = "unload/copia_QPC.gif";
+  cr_load(3, file1);
+  cr_load(3, file2);
+  printf("------------------ CR LS Particion 3 ------------------ \n");
+  cr_ls(3);
+
+  printf("------------------ Ahora copiamos una carpeta ------------------ \n");
+  cr_ls(3);
+  destroy_directories();
+  destroy_bitmaps();
 
   return 0;
 }
