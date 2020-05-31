b8950996893dee9c94750202f73a843fb9bc4a1b
ClaudioPrieto <cjprieto@uc.cl>
2020-05-31 18:22:32 -0400

    test separados

--- src/crfs/main.c
+++ src/crfs/main.c
@@ -4,7 +4,6 @@
 #include <string.h>
 #include "cr_API.h"
 
-//TEST 4
 
 int main(int argc, char *argv[]) {
   if (argc != 2)
@@ -24,62 +23,6 @@ int main(int argc, char *argv[]) {
   create_dir_blocks();
   create_cr_bitmaps();
 
-  // TEST 5
-  // cr_ls(1);
-  // printf("\n\n\n");
-
-  // crFILE* arch = cr_open(1, "guides.txt","r");
-  // char* buffer = calloc(1001, sizeof(char));
-  // int num = cr_read(arch, buffer, 1000);
-  // printf("num bytes leidos = %d\n", num);
-  // printf("Extracto 1 = %s\n", buffer);
-  // printf("\n\n\n");
-  // cr_close(arch);
-  // free(buffer);
-
-  // cr_hardlink(1, "guides.txt", "guides_copy.txt");
-  
-  // cr_ls(1);
-  // printf("\n\n\n");
-
-  // crFILE* arch2 = cr_open(1, "guides.txt","r");
-  // char* buffer2 = calloc(1001, sizeof(char));
-  // int num2 = cr_read(arch2, buffer2, 1000);
-  // printf("num bytes leidos = %d\n", num2);
-  // printf("Extracto 1 = %s\n", buffer2);
-  // printf("\n\n\n");
-  // cr_close(arch2);
-  // free(buffer2);
-  
-  // TEST 6
-  // printf("Archivos en particion 1\n");
-  // cr_ls(2);
-  // printf("\n\n");
-  // printf("Archivos en particion 2\n");
-  // cr_ls(2);
-  // printf("\n\n");
-  // cr_soflink(1, 2, "guides_copy.txt");
-
-  // printf("Archivos en particion 1\n");
-  // cr_ls(1);
-  // printf("\n\n");
-  // printf("Archivos en particion 1\n");
-  // cr_ls(2);
-  // printf("\n\n");
-  // cr_rm(2, "guides.txt");
-
-
-  // crFILE* arch = cr_open(2, "1/guides_copy.txt","r");
-  // char* buffer = calloc(1001, sizeof(char));
-  // int num = cr_read(arch, buffer, 1000);
-  // printf("num bytes leidos = %d\n", num);
-  // printf("Extracto 1 = %s\n", buffer);
-  // printf("\n\n\n");
-  // cr_close(arch);
-  // free(buffer);
-
-
-  
 
   destroy_directories();
   destroy_bitmaps();
