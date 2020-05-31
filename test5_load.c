790bfdd4e0329feec5299020922dbe657f9bf0cd
Diiru <diiru@uc.cl>
2020-05-31 05:13:43 -0400

    init load test

---
+++ test5_load.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "cr_API.h"
+
+//TEST 4
+
+int main(int argc, char *argv[]) {
+  if (argc != 2)
+  {
+    printf("Input debe coindicir con: ./crfs <simdisk.bin>\n");
+    return 0;
+  }
+  char* diskname = argv[1];
+
+  cr_mount(diskname);
+
+  if (!ruta_archivo) {
+    printf("Disk not mounted");
+    return 0;
+  }
+
+  create_dir_blocks();
+  create_cr_bitmaps();
+
+
+  destroy_directories();
+  destroy_bitmaps();
+
+  return 0;
+}
