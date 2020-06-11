ac51904ff2b5694530fc30e9a1cc1c7017793f49
Diiru <diiru@uc.cl>
2020-06-11 19:48:57 -0400

    test 8

--- test_8_main.c
+++ test_8_main.c
@@ -23,11 +23,6 @@ int main(int argc, char *argv[]) {
 
   create_dir_blocks();
   create_cr_bitmaps();
-  //cr_bitmap(4,1);
-  //crFILE* arch = cr_open(4, "Orgullo y Prejuicio2.txt","w");
-  //cr_close(arch);
-  //cr_bitmap(4,0);
-  //cr_bitmap(0, 1);
   printf("-----------------------------------------------------------------------\n");
   printf("------------------------ TEST funciones LOAD ------------------------ \n");
   printf("-----------------------------------------------------------------------\n");
@@ -40,14 +35,18 @@ int main(int argc, char *argv[]) {
   cr_ls(disk);
   printf("------------------------ FIN CR LS ------------------------\n");
   printf("2. Que archivo deseas subir ?\n");
-  char archivo[32];
-  scanf("%s", archivo);
+  char file[29];
+  scanf("%s", file);
+  char* archivo = malloc(sizeof(char)*30);
+  memcpy(archivo, file, 29);
   cr_load(disk, archivo);
   printf("3. Archivo: %s copiado\n", archivo);
   printf("------------------------ CR LS PARTICION %u ------------------------\n", disk);
   cr_ls(disk);
   printf("------------------------ FIN CR LS ------------------------\n");
-
+  //crFILE* fl =cr_open(4,file, "r");
+  //cr_close(fl);
+  free(archivo);
   printf("-----------------------------------------------------------------------\n");
   printf("Funcionalidad 2: Subir una carpeta a cierta particion\n");
   printf("A que particion desea copiar la carpeta? (1-4):");
@@ -56,16 +55,16 @@ int main(int argc, char *argv[]) {
   printf("------------------------ CR LS PARTICION %u ------------------------\n", disk2);
   cr_ls(disk2);
   printf("------------------------ FIN CR LS ------------------------\n");
-  char carpeta[32];
-  printf("Que directorio quiere leer?: (/load/)\n");
-  scanf("%s", carpeta);
+  char* carpeta = malloc(sizeof(char)*30);
+  memcpy(carpeta, "/load/", 29);
+  printf("Que directorio quiere leer?: (/load/): \n");
   printf("3. Carpeta: %s copiada\n", carpeta);
   cr_load(disk2, carpeta);
   printf("------------------------ CR LS PARTICION %u ------------------------\n", disk2);
   cr_ls(disk2);
   printf("------------------------ FIN CR LS ------------------------\n");
   printf("-----------------------------------------------------------------------\n");
-
+  free(carpeta);
   destroy_directories();
   destroy_bitmaps();
 
