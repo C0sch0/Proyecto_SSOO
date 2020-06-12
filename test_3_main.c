e80ebad8b39b7dd012160e8854bff0fae95fc6be
Tamara <tylues@uc.cl>
2020-06-12 12:41:30 -0400

    prints test 3-4

--- test_3_main.c
+++ test_3_main.c
@@ -22,34 +22,60 @@ int main(int argc, char *argv[]) {
 
   create_dir_blocks();
   create_cr_bitmaps();
+
+  printf("-----------------------------------------------------------------------\n");
+  printf("------------------------ TEST funciones OPEN ------------------------ \n");
+  printf("-----------------------------------------------------------------------\n");
+
+  printf("1. Abriremos el archivo 'Baroque.mp3' de la partición 1 en formato de lectura\n");
+  printf("Funcionalidad a probar: Archivos abiertos en formato de lectura no se pueden escribir\n");
+
   crFILE* arch = cr_open(1, "Baroque.mp3","r");
   FILE* prueba = fopen("prueba.txt", "r");
   char* texto = calloc(14000, sizeof(char));
   fgets(texto, 10000, prueba);
+
   if(arch != NULL){
+    printf("Intentaremos escribir en 'Baroque.mp3'\n");
     int num = cr_write(arch, texto, 14000);
-    cr_close(arch);  
+    cr_close(arch);
   }
+
   free(texto);
   fclose(prueba);
 
+  printf("2. Abriremos un archivo con el nombre 'Baroque444.mp3' en la partición 1 en formato de escritura\n");
+  printf("Funcionalidad a probar: Archivos abiertos en formato de escritura no se pueden leer\n");
+
   crFILE* arch2 = cr_open(1, "Baroque444.mp3","w");
   char* buffer = calloc(1001, sizeof(char));
+
   if(arch2 != NULL){
+    printf("Intentaremos leer en 'Baroque444.mp3'\n");
     int num2 = cr_read(arch2, buffer, 1000);
     cr_close(arch2);
   }
   free(buffer);
-  
+
+  printf("3. Abriremos un archivo con el nombre 'Baroque.mp3' en la partición 1 en formato de escritura\n");
+  printf("Funcionalidad a probar: Archivos que ya existen no peuden ser abiertos en formato de escritura\n");
+
+
   crFILE* arch3 = cr_open(1, "Baroque.mp3","w");
   if(arch3 != NULL){
     cr_close(arch3);
   }
-  
+
+  printf("4. Abriremos el archivo con el nombre '3/Baroque.mp3' de la partición 4 en formato de lectura\n");
+  printf("Funcionalidad a probar: Archivos que son un 'broken link' no pueden ser abiertos\n");
+
   crFILE* arch4 = cr_open(4, "3/Baroque.mp3","r");
   if(arch4 != NULL){
     cr_close(arch4);
   }
+
+  printf("5. Abriremos un archivo con el nombre 'Baroque.mp3' en la partición 4 en formato de escritura\n");
+  printf("Funcionalidad a probar: Archivos que no existen en la particion no pueden ser abiertos\n");
 
   crFILE* arch5 = cr_open(4, "Baroque.mp3","r");
   if(arch5 != NULL){
--- test_4_main.c
+++ test_4_main.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
     printf("Input debe coindicir con: ./crfs <simdisk.bin>\n");
     return 1;
   }
-  char* diskname = argv[1]; 
+  char* diskname = argv[1];
 
   cr_mount(diskname);
   if (!ruta_archivo) {
@@ -22,31 +22,62 @@ int main(int argc, char *argv[]) {
 
   create_dir_blocks();
   create_cr_bitmaps();
+
+  printf("-----------------------------------------------------------------------\n");
+  printf("------------------------ TEST funciones Write ------------------------ \n");
+  printf("-----------------------------------------------------------------------\n");
+
+
+  printf("Funcionalidades a probar: Correcta escritura y lectura en el disco.\n");
+
+  printf("1. Abrimos un archivo en modo de lectura'\n");
+
   crFILE* arch = cr_open(4, "Orgullo y Prejuicio.txt","w");
+
+  printf("2. Creamos un buffer con un extracto del libro 'Orgullo y Prejuicio'\n");
   FILE* prueba = fopen("prueba.txt", "r");
   char* texto = calloc(14000, sizeof(char));
   fgets(texto, 10000, prueba);
+
+  printf("3. Escribimos el buffer en el archivo creado\n");
   int num = cr_write(arch, texto, 14000);
   free(texto);
   fclose(prueba);
   cr_close(arch);
+
+  printf("4. Cerramos el archivo creado y lo abrimos en modo de lectura\n");
   crFILE* arch2 = cr_open(4, "Orgullo y Prejuicio.txt","r");
+
+  printf("5. Creamos un buffer de 1000 bytes\n");
   char* buffer = calloc(1001, sizeof(char));
+
+  printf("6. Leemos 1000 bytes del archivo\n");
   int num2 = cr_read(arch2, buffer, 1000);
   printf("num bytes leidos = %d\n", num2);
   printf("Extracto 1 = %s\n", buffer);
   printf("\n\n\n");
+
+  printf("7. Creamos un nuevo buffer de 1000 bytes\n");
   char* buffer2 = calloc(1001, sizeof(char));
+
+  printf("8. Leemos 1000 bytes del archivo partiendo del punto en que quedó la lectura anterior\n");
   int num3 = cr_read(arch2, buffer2, 1000);
   printf("num bytes leidos = %d\n", num3);
   printf("Extracto 2 = %s\n", buffer2);
   printf("\n\n\n");
+
+  printf("9. Creamos un nuevo buffer de 7000 bytes\n");
   char* buffer3 = calloc(7001, sizeof(char));
+
+  printf("10. Leemos 7000 bytes del archivo partiendo del punto en que quedó la lectura anterior\n");
   int num4 = cr_read(arch2, buffer3, 7000);
   printf("num bytes leidos = %d\n", num4);
   printf("Extracto 3 = %s\n", buffer3);
   printf("\n\n\n");
+
+  printf("11. Creamos un último buffer 1000 bytes\n");
   char* buffer4 = calloc(1001, sizeof(char));
+  printf("12. Leemos 1000 bytes del archivo partiendo del punto en que quedó la lectura anterior\n")
   int num5 = cr_read(arch2, buffer4, 1000);
   printf("num bytes leidos = %d\n", num5);
   printf("Extracto 4 = %s\n", buffer4);
